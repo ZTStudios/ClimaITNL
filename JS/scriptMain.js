389d8b9f827ea1fc5853086d0f2d60ec3397be05
BryanTurrubiates <bryanturrubiates@outlook.com>
2022-06-16 18:58:02 -0500

    Version Alpha 1.1.3 Se agregaron condiciones para el cambio dinamico de Clima

--- JS/scriptMain.js
+++ JS/scriptMain.js
@@ -1,34 +1,40 @@
 window.onload = () => {
     
     console.log('Archivo Cargado')
+        
+    let LinkA = 'https://api.openweathermap.org/data/2.5/weather?lat=65.41506&lon=-52.89822&units=metric&lang=sp&appid=75388a5617c4890016f8215a20d3ac3f';
     
-    function CargarWallpaper () {
-        
-        let tiempoActual = new Date();
-        console.log(tiempoActual.getHours())
-        
-        if ( tiempoActual.getHours() >= 8 && tiempoActual.getHours() < 20 ) {
-            
-            
-            document.body.style.backgroundImage = "url('../Resource/WallapaperState/LluviaWall.png')"
-            
-        }
-    }
-
-    CargarWallpaper();
-
     
-    function ApiLol () {
+    async function CargarWallpaper() {
         
-        fetch('https://api.openweathermap.org/data/2.5/weather?lat=65.41506&lon=-52.89822&units=metric&lang=sp&appid=75388a5617c4890016f8215a20d3ac3f')
-        .then(response => {
-            return response.json();
-        })
-        .then(response =>  ClimaState = response.weather[0].main);
+        let HoraActual = new Date();
+        var ObjJson = "initial";
+
+        await fetch(LinkA)
+            .then(res => res.json())
+            .then(data => {
+                ObjJson = data;
+                console.log(ObjJson);
+
+                if(HoraActual.getHours() >= 8  && HoraActual.getHours() < 20) {
+
+                    if (ObjJson.weather[0].main == 'Clear') {
+                        document.body.style.backgroundImage = "url('../Resource/WallapaperState/SoleadoWall.png')"
+                    }
+                    if (ObjJson.weather[0].main == 'Clouds') {
+                        document.body.style.backgroundImage = "url('../Resource/WallapaperState/NubladoWall.png')"
+                    }
+                    if (ObjJson.weather[0].main == 'Rain'){
+                        document.body.style.backgroundImage = "url('../Resource/WallapaperState/LluviaWall.png')"
+                    }
+
+                } 
+                
+                
+            })
+
+              
     }
-
-
-    ApiLol()
 
     
     
