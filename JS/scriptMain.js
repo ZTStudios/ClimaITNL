b1015118d40ee9aeb9ab16e01ae302ded1a6f0ff
AndyIbarraVG <AndyIbarraVG@gmail.com>
2022-06-16 19:27:05 -0500

    Version Alpha 1.2 Se agregaron las demas condiciones para el cambio dinamico del Clima

--- JS/scriptMain.js
+++ JS/scriptMain.js
@@ -2,7 +2,7 @@ window.onload = () => {
     
     console.log('Archivo Cargado')
         
-    let LinkA = 'https://api.openweathermap.org/data/2.5/weather?lat=65.41506&lon=-52.89822&units=metric&lang=sp&appid=75388a5617c4890016f8215a20d3ac3f';
+    let LinkA = 'https://api.openweathermap.org/data/2.5/weather?lat=27.48&lon=-99.5105&units=metric&lang=sp&appid=75388a5617c4890016f8215a20d3ac3f';
     
     
     async function CargarWallpaper() {
@@ -27,14 +27,20 @@ window.onload = () => {
                     if (ObjJson.weather[0].main == 'Rain'){
                         document.body.style.backgroundImage = "url('../Resource/WallapaperState/LluviaWall.png')"
                     }
-
-                } 
-                
-                
-            })
-
-              
+                    if (ObjJson.weather[0].main == 'Thunderstorm'){
+                        document.body.style.backgroundImage = "url('../Resource/WallapaperState/TormentaWall.png')"
+                    }
+                    if (ObjJson.weather[0].main == 'Snow'){
+                        document.body.style.backgroundImage = "url('../Resource/WallapaperState/NevadoWall.png')"
+                    } 
+                }
+                else{
+                    document.body.style.backgroundImage = "url('../Resource/WallapaperState/NocheWall.png')"
+                }
+            })    
     }
+
+    CargarWallpaper()
 
     
     
